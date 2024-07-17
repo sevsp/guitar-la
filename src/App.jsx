a1f811a349dba512550939ee12fe3968c071b3d7
Sebastián <sebsmenprofessional467@gmail.com>
2024-07-16 23:07:25 -0400

    props was created

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,11 @@ function App() {
             <h2 className="text-center">Nuestra Colección</h2>
 
             <div className="row mt-5">
-                {data.map(() => (
-                    <Guitar />
+                {data.map((guitar) => (
+                    <Guitar 
+                        key={guitar.id}
+                        guitar={guitar}
+                    />
                 ))}
             </div>
         </main>
