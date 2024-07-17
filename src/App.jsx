c4a243ef313955739c0f000820841e792d9b3a65
Sebastián <sebsmenprofessional467@gmail.com>
2024-07-16 22:46:02 -0400

    Develop changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-
+import Header from "./components/Header"
+import Guitar from "./components/Guitar"
+import { db } from "./data/db"
+import { useState } from "react"
 
 function App() {
-  
-  return (
-    <>
+    
+    const [data, setData] = useState(db)
 
+    return (
+    <>
+        <Header />
+        <main className="container-xl mt-5">
+            <h2 className="text-center">Nuestra Colección</h2>
+
+            <div className="row mt-5">
+                {data.map(() => (
+                    <Guitar />
+                ))}
+            </div>
+        </main>
+
+
+        <footer className="bg-dark mt-5 py-5">
+            <div className="container-xl">
+                <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+            </div>
+        </footer>
     </>
-  )
+    )
 }
 
 export default App
